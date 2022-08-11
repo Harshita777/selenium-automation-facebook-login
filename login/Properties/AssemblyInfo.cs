3320c394de57dca5f340a2cc790de98907e92504
Harshita <Harshita@DESKTOP-VP8D6ID>
2022-08-11 23:03:02 +0530

    Add project files.

---
+++ login/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("login")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("login")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5f3dd576-63b9-47cb-a83b-a645de9abed8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ login/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace selenium_test_1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void demo()
+        {
+
+            Console.WriteLine("Test is Started");
+
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://facebook.com");
+
+            Thread.Sleep(2000);
+
+        
+
+            IWebElement eleSessionKey = driver.FindElement(By.Name("email"));
+
+            IWebElement elePassword = driver.FindElement(By.Name("pass"));
+
+            eleSessionKey.SendKeys("username");
+
+            Thread.Sleep(2000);
+            elePassword.SendKeys("password");
+            IWebElement eleSubmitBtn = driver.FindElement(By.Name("login"));
+            Thread.Sleep(1000);
+            eleSubmitBtn.Click();
+
+            Thread.Sleep(55000);
+
+            driver.Close();
+            Console.WriteLine("Test is Ended ");
+        }
+        
+    }
+}
